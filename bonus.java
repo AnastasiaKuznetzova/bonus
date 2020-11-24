bef33dadc01a5dc5e525b4b3f009284a63d3fc27
AnastasiaKuznetsova <you@example.com>
2020-11-24 23:05:28 +0300

    new

---
+++ bonus.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main (String[] args) {
+        int valueInKpoecks = 100000;
+        int paymentInKopecks = 180000;
+        int bonusInKopecks;
+        if (paymentInKopecks > valueInKpoecks) {
+            bonusInKopecks = paymentInKopecks / 10000;
+        } else {
+            bonusInKopecks = 0;
+        }
+
+        System.out.println(bonusInKopecks);
+
+    }
+}
